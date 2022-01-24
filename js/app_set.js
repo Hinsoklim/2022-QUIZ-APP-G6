01cbee023fa3c534753dce89d125d30f1bbac424
Soklim.HIN <soklim.hin@PNC.LAN>
2022-01-24 13:15:42 +0700

    Data Struture Form

--- js/app_set.js
+++ js/app_set.js
@@ -1,18 +1,43 @@
-// Get Value input 
 
-// Value Question 
-let valueQuestion = document.getElementById('quest').value;
-// Value Answer1
-let An1 = document.getElementById('option1').value;
-let An2 = document.getElementById('option2').value;
-let An3 = document.getElementById('option3').value;
-let An4 = document.getElementById('option4').value;
-
-// container Add quiz 
-let cardQuiz = document.getElementsByClassName('card-form-created');
 
 // Function ADD Quiz
-function addQuizCard(){
+function StoreDataInput(storeData){
+    storeData = []
+    questionStore = {};
+    answerStore = {};
+
+    // Get Value input Form
+    let inputQuestion = document.getElementById('quest').value;
+    let inputAn1 = document.getElementById('option1').value;
+    let inputAn2 = document.getElementById('option2').value;
+    let inputAn3 = document.getElementById('option3').value;
+    let inputAn4 = document.getElementById('option4').value;
+    questionStore['Question'] = inputQuestion;
+    
+    answerStore['answer1'] = inputAn1;
+    answerStore['answer2'] = inputAn2;
+    answerStore['answer3'] = inputAn3;
+    answerStore['answer4'] = inputAn4;
+
+    storeData.push(questionStore);
+    storeData.push(answerStore);
+
     let form = document.createElement('form');
-    let lable = document.createElement('label');
+    
+
+
+    // localStorage.setItem('form',storeData);
+    
+    
 }
+
+
+
+
+
+// Event Button
+let btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener('click',StoreDataInput);
+
+
+
