09f747548e78c6778d99fda63918b32dd6487b1e
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-01-25 20:30:31 +0700

    #2 add delete

--- js/edit_quiz.js
+++ js/edit_quiz.js
@@ -1,67 +1,83 @@
 
 // create  a list of questions
 let all_questions = [
-{question:'Whic is the short cut langege?',
-answer:{ans1:"Js",ans2:"Html",ans3:"Css",ans4:"Php"},
-crrectAnswer:"ans1"
-},
-{question:' Which is not valid data type in Javascript ',
-answer:{ans1:"Undefinded",ans2:"Undefinded",ans3:"float",ans4:"Number"},
-crrectAnswer:"ans1"
-},
-{question:'Which type of language is Javascript',
-answer:{ans1:" Programming",ans2:"HtScriptingml",ans3:"Markup",ans4:" None of the above"},
-crrectAnswer:"ans1"
-},
-{question:'By which javascript code is used',
-answer:{ans1:"Classes",ans2:" Methods/Functions",ans3:"RMI",ans4:"Events"},
-crrectAnswer:"ans1"
-},
-{question:'Which tag is used to write the javascript code',
-answer:{ans1:"<script>",ans2:"<sp>",ans3:" <javascript>",ans4:"<java>"},
-crrectAnswer:"ans1"
-},
+    {
+        title: 'What is your favorite teacher and most handsome?',
+        answers: { ans1: "rady", ans2: "him", ans3: "hyacinthe", ans4: "ronan" },
+        correctAnswer: "ans4"
+    },
+    {
+        title: ' Which is not valid data type in Javascript ',
+        answers: { ans1: "Undefinded", ans2: "Undefinded", ans3: "float", ans4: "Number" },
+        correctAnswer: "ans2"
+    },
+    {
+        title: 'Which type of language is Javascript',
+        answers: { ans1: " Programming", ans2: "HtScriptingml", ans3: "Markup", ans4: " None of the above" },
+        correctAnswer: "ans1"
+    },
+    {
+        title: 'By which javascript code is used',
+        answers: { ans1: "Classes", ans2: " Methods/Functions", ans3: "RMI", ans4: "Events" },
+        correctAnswer: "ans1"
+    },
+    {
+        title: 'Which tag is used to write the javascript code',
+        answers: { ans1: "<script>", ans2: "<sp>", ans3: " <javascript>", ans4: "<java>" },
+        correctAnswer: "ans1"
+    },
 
 ]
 
 // creqte a function to display questions
-function displayQuestion(){
+function displayQuestion() {
     // Mey
-    for(let allQuetoins of all_questions ){
+    for (let allQuetoins of all_questions) {
 
-    let cardForm =document.querySelector('.card-form-created');
-    let ol = document.createElement('ol');
-    let div = document.createElement('div');
-    div.setAttribute('id','delete');
-    // cardForm.appendChild(ol);
-    let p = document.createElement('p');
-    p.textContent = allQuetoins['question'];
-    div.appendChild(p);
-    ol.appendChild(div);
-    cardForm.appendChild(ol);
-    choiceAnswer = allQuetoins['answer']
-    
-    for(let allAnswer in choiceAnswer){
-        let li = document.createElement('li');
-        let label = document.createElement('label');
-        let radio = document.createElement('input');
+        let cardForm = document.querySelector('.card-form-created');
+        let ol = document.createElement('ol');
+        let div = document.createElement('div');
+        div.setAttribute('id', 'delete');
+        // cardForm.appendChild(ol);
+        let p = document.createElement('p');
+        p.textContent = allQuetoins['title'];
+        div.appendChild(p);
+        ol.appendChild(div);
+        cardForm.appendChild(ol);
+        choiceAnswer = allQuetoins['answers']
+        trueAnswer = allQuetoins['correctAnswer']
         
-        radio.type = 'radio';
-        radio.name = 'definition';
-        label.textContent = choiceAnswer[allAnswer];
-        li.appendChild(radio);
-        li.appendChild(label);
-        ol.appendChild(li)
-        console.log(cardForm)
-    }
-    let i = document.createElement('i');
-    i.className = 'fa fa-trash'
-    div.appendChild(i);
-}
-}
-displayQuestion()
 
-function addQuestion(){
+        for (let allAnswer in choiceAnswer) {
+            let li = document.createElement('li');
+            let label = document.createElement('label');
+            let radio = document.createElement('input');
+            radio.type = 'radio';
+            radio.name = 'definition';
+            label.textContent = choiceAnswer[allAnswer];
+            li.appendChild(radio);
+            li.appendChild(label);
+            div.appendChild(li)
+            
+         
+        
+        }
+        let i = document.createElement('i');
+        i.className = 'fa fa-trash';
+        ol.appendChild(i);
+        
+        
+    }
+}
+
+displayQuestion()
+function removeQuestion(event){
+    if (event.target.className === 'fa fa-trash'){
+        event.target.parentElement.remove()
+      }
+    console.log(event.target.className);
+}
+function addQuestion() {
     // Soklim
     // Get Value Input
     let inputQuestion = document.getElementById('quest').value;
@@ -69,7 +85,7 @@ function addQuestion(){
     let inputAn2 = document.getElementById('option2').value;
     let inputAn3 = document.getElementById('option3').value;
     let inputAn4 = document.getElementById('option4').value;
-    
+
     let newQuestion = {};
     let answer = [];
     let Answer1 = {};
@@ -96,21 +112,21 @@ function addQuestion(){
     console.log(questions);
     displayQuestion(questions);
     setInLocalStorage(questions);
-    
-    
-    
+
+
+
 }
 
-function setInLocalStorage(questions){
+function setInLocalStorage(questions) {
 
-    localStorage.setItem('question',JSON.stringify(questions));
+    localStorage.setItem('question', JSON.stringify(questions));
 }
 
 questions = [];
 
 // Event Button
 const btnAdd = document.getElementById('btnAdd');
-btnAdd.addEventListener('click',addQuestion);
+btnAdd.addEventListener('click', addQuestion);
 
-
-
+const btnRemove =document.querySelector('ol');
+btnRemove.addEventListener('click',removeQuestion);
