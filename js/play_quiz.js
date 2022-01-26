5db6ab5d8bc03313b17b3d7a59ab6dd10fd98147
Phanith.CHHIM <Phanith.CHHIM@PNC.LAN>
2022-01-26 20:01:26 +0700

    #4 Display Question

--- js/play_quiz.js
+++ js/play_quiz.js
@@ -1,7 +1,38 @@
+// create a function to display questions
+function displayQuestion(){
 
+    let newData = JSON.parse(localStorage.getItem('question'));
+    let quizContainer = document.getElementById('quizContainer');
+    let div = document.createElement('div');
+    div.className = 'quizForm';
+    document.body.appendChild(div);
+    let old = document.querySelectorAll('.quizForm');
+    if (old.length >1){
+        old[0].remove();
+    }
+    for (let questionQuiz of newData){
+        let form = document.createElement('form');
+        let p = document.createElement('p');
+        p.textContent = questionQuiz.question;
+        form.appendChild(p);
+        div.appendChild(form);
+        for(ans of questionQuiz.answer){
+            let optionAn = document.createElement('input');
+            let br = document.createElement('br');
+            let label = document.createElement('label');
+            optionAn.type = 'radio';
+            optionAn.name = 'answer';
+            label = textContent = ans.option;
+            form.append(optionAn);
+            form.append(label);
+            form.append(br);
 
+            div.appendChild(form);
+            quizContainer.append(div);
+        }
+    }
 
-
+}
 function playQuiz(){
     let btnPlay = document.getElementById('btnPlayQuiz');
     btnPlay.style.display = 'none';
